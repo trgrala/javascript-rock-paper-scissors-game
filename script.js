81c27b216c4a72d2ad4556df231f2ba091291a37
trgrala <bocchifan38@gmail.com>
2026-02-06 18:21:57 +0100

    Replaced the console logs with html msg.  - game can now be played in browser  - after reaching 5 points the game is over  - no reset button for now

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 let playerScore = 0;
 let computerScore = 0;
+const playerScoreMsg = document.querySelector('#playerScore');
+const computerScoreMsg = document.querySelector('#computerScore');
+const playerIcon = document.querySelector('#playerIcon');
+const computerIcon = document.querySelector('#computerIcon');
+const roundMsg = document.querySelector('#roundMsg');
+const gameMsg = document.querySelector('#gameMsg');
+const winMsg = document.querySelector('#winMsg');
 
 const buttons = document.querySelectorAll(".btn-choice");
 
@@ -23,25 +30,49 @@ function playRound(playerChoice,computerChoice){
     console.log('Player vs Computer');
 
     if(playerChoice===computerChoice)
-        console.log('Tie!');
+        roundMsg.textContent = "It's a TIE!";
+
+        // console.log('Tie!');
     else if(
         (playerChoice === 'ROCK' && computerChoice === 'SCISSORS') ||
         (playerChoice === 'PAPER' && computerChoice === 'ROCK') ||
         (playerChoice === 'SCISSORS' && computerChoice === 'PAPER')
     ){
-        console.log('Player: '+playerChoice)
-        console.log('Computer: '+computerChoice)
-        console.log('----------------------------')
-        console.log('Player Wins!');
+        playerIcon.textContent = 'Player choice:'+playerChoice;
+        computerIcon.textContent = 'Computer choice:'+computerChoice;
+        roundMsg.textContent = 'Player Wins!';
         playerScore++
+        playerScoreMsg.textContent = playerScore;
+
+        // console.log('Player: '+playerChoice)
+        // console.log('Computer: '+computerChoice)
+        // console.log('----------------------------')
+        // console.log('Player Wins!');
+        // playerScore++
     }
     else{
-        console.log('Player: '+playerChoice)
-        console.log('Computer: '+computerChoice)
-        console.log('----------------------------')
-        console.log('Computer Wins!')
+        playerIcon.textContent = 'Player choice:'+playerChoice;
+        computerIcon.textContent = 'Computer choice:'+computerChoice;
+        roundMsg.textContent = 'Computer Wins!';
         computerScore++
+        computerScoreMsg.textContent = computerScore;
+
+        // console.log('Player: '+playerChoice)
+        // console.log('Computer: '+computerChoice)
+        // console.log('----------------------------')
+        // console.log('Computer Wins!')
+        // computerScore++
     }
 
-    console.log('Player Score: '+playerScore+' - '+'Computer Score: '+computerScore)
+    gameMsg.textContent = 'Player Score: '+playerScore+' | '+'Computer Score: '+computerScore;
+
+    isGameOver();
+}
+
+function isGameOver(){
+    if(playerScore === 5){
+        winMsg.textContent = 'Player wins the game!';
+    }else if(computerScore === 5){
+        winMsg.textContent = 'Computer wins the game!';
+    }
 }
